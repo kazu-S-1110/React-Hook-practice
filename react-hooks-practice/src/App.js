863739efa38eeacade3e90d4df67babf93cf4981
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2020-10-18 03:38:47 +0900

    複数のStateの管理を学習

--- react-hooks-practice/src/App.js
+++ react-hooks-practice/src/App.js
@@ -1,44 +1,62 @@
 import React, { useState } from 'react';
 import styled from "styled-components";
 
-const App = () => {
-  const [count, setCount] = useState(0)
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
-  const reset = () => setCount(0)
+const App = (props) => {
+  // 初期値をここで設定するパターン
+  // const initialStates = { name: "", price: 1000 }
+
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
+
+
   return (
     <>
-      <Title>count: {count}</Title>
       <Container>
-        <Button onClick={increment}>+1</Button>
-        <Button onClick={decrement}>-1</Button>
-        <Button onClick={reset}>reset</Button>
+        <Title>現在の{name}は、{price}円です</Title>
+        <Button onClick={() => setPrice(price + 1)}>+1</Button>
+        <Button onClick={() => setPrice(price - 1)}>-1</Button>
+        <Button onClick={reset}>Reset</Button>
+        <input value={name} onChange={e => setName(e.target.value)}></input>
       </Container>
+
     </>
-  );
+
+  )
+}
+
+// 初期値を外部より取り込むパターン
+App.defaultProps = {
+  name: "sample",
+  price: 1000
 }
 
 const Title = styled.h2`
   font-size:50px;
   background-color:cyan;
   margin: 0;
-  position: absolute;
-  top: 50%;
+  position: relative;
+  /* top: 50%;
   left: 50%;
   margin-right: -50%;
-  transform:translate(-50%,-50%);
+  transform:translate(-50%,-50%); */
 `
 const Container = styled.div`
   display:flex;
   position:absolute;
-  width: 400px;
-  height: 100px;
-  top: 60%;
+  width: 500px;
+  height: 500px;
+  top: 50%;
   left:50%;
   transform:translate(-50%,-50%);
-  /* outline:#000 solid 1px; */
+  outline:#000 solid 1px;
   justify-content:center;
   align-items:center;
+  flex-direction:column;
+
 `
 
 const Button = styled.button`
