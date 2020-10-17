54d615c27970a865b421dd9bb673a056214bd341
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2020-10-18 03:14:40 +0900

    Learn useState

--- react-hooks-practice/src/App.js
+++ react-hooks-practice/src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import styled, { isStyledComponent } from "styled-components";
-
+import React, { useState } from 'react';
+import styled from "styled-components";
 
 const App = () => {
+  const [count, setCount] = useState(0)
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+  const reset = () => setCount(0)
   return (
-    <Title>
-      This is a template for React app.
-    </Title>
+    <>
+      <Title>count: {count}</Title>
+      <Container>
+        <Button onClick={increment}>+1</Button>
+        <Button onClick={decrement}>-1</Button>
+        <Button onClick={reset}>reset</Button>
+      </Container>
+    </>
   );
 }
 
@@ -19,6 +27,25 @@ const Title = styled.h2`
   left: 50%;
   margin-right: -50%;
   transform:translate(-50%,-50%);
+`
+const Container = styled.div`
+  display:flex;
+  position:absolute;
+  width: 400px;
+  height: 100px;
+  top: 60%;
+  left:50%;
+  transform:translate(-50%,-50%);
+  /* outline:#000 solid 1px; */
+  justify-content:center;
+  align-items:center;
+`
+
+const Button = styled.button`
+  width: auto;
+  margin-right:20px;
+  background-color:green;
+  font-size:20px;
 `
 
 
