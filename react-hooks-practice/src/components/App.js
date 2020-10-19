dd9f41e2a85ae92ea54901c5c945307e2d1d26a7
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2020-10-19 12:09:16 +0900

    display events

--- react-hooks-practice/src/components/App.js
+++ react-hooks-practice/src/components/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useReducer, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import Event from "./event.js"
+import reducer from "../reducers/index"
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, [])
+  const [title, setTitle] = useState("")
+  const [body, setBody] = useState("")
+  const addEvent = e => {
+    e.preventDefault()
+    // console.log({title, body}) アクセスが可能か確認
+    dispatch({ //この式でイベントを追加可能
+      type: "CREATE_EVENT",
+      title,
+      body
+    })
+    setTitle("")
+    setBody("")
+  }
+
   return (
     <>
       <div className="container-fluid mt-5">
@@ -12,29 +27,31 @@ const App = () => {
 
           <div className="form-group">
             <label htmlFor="formEventTitle">タイトル</label>
-            <input type="text" className="form-control" id="formEventTitle"/>
+            <input type="text" className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)}/>
           </div>
 
           <div className="form-group">
-            <label htmlFor="formEventBody">メッセージ</label>
-            <textarea type="text" className="form-control" id="formEventBody"/>
+            <label htmlFor="formEventBody">ボディー</label>
+            <textarea type="text" className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)}/>
           </div>
 
-          <button className="btn btn-primary">イベントを作成する</button>
+          <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
           <button className="btn btn-danger">全てのイベントを削除する</button>
 
         </form>
 
-        <h4>イベント一覧</h4>
+        <h4 className="mt-4">イベント一覧</h4>
         <table className="table table-hover">
           <thead>
             <tr>
-              <th>ID</th>
+              <th>id</th>
               <th>タイトル</th>
               <th>メッセージ</th>
             </tr>
           </thead>
           <tbody>
+            {state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch}/>))}
+            
           </tbody>
         </table>
       </div>
