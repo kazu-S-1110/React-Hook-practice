e32facd744be16a6173e26cbf59af8862a3ef4b1
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2020-10-20 10:35:50 +0900

    setup react context provider

--- react-hooks-practice/src/components/App.js
+++ react-hooks-practice/src/components/App.js
@@ -1,20 +1,20 @@
-import React, { useReducer } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useReducer } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import EventForm from "./EventForm"
 import Events from "./Events"
 import reducer from "../reducers/index"
-
+import AppContext from "../contexts/AppContext"
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, [])
   return (
-    <>
+    <AppContext.Provider value={"Hello , this is a Context"}>
       <div className="container-fluid mt-5">
         <EventForm state={state} dispatch={dispatch}/>
         <Events state={state} dispatch={dispatch}/>
       </div>
-    </>
-  );
+    </AppContext.Provider>
+  )
 }
 
 
--- react-hooks-practice/src/components/Events.js
+++ react-hooks-practice/src/components/Events.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useContext }　 from 'react'
 import Event from "./event"
+import AppContext from "../contexts/AppContext"
 
-const Events = ({state, dispatch}) => {
+
+
+const Events = ({ state, dispatch }) => {
+  const value = useContext(AppContext) {/* <= useContextの場合 */} 
   return (
     <>
+      {/* 以下のConsumerの記述は古いのでuseContextで書いた方がスマート */}
+      {/* <AppContext.Consumer >
+        {value => <div className="mt-5">{value}</div>}
+      </AppContext.Consumer> */}
+
+      <div className="mt-4">{value}</div> {/* <= useContextの場合 */} 
       <h4 className="mt-5">イベント一覧</h4>
         <table className="table table-hover">
           <thead>
