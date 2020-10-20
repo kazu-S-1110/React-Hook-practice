b293e91e10ed03f84270e4f424f43c8e08694566
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2020-10-20 10:16:05 +0900

    create one react context

--- react-hooks-practice/src/actions/index.js
+++ react-hooks-practice/src/actions/index.js
@@ -1,3 +1,5 @@
+//typeがストリングのままだとエラーを吐かないけど正常に動作しないことありのため、変数に格納する。そうすることでタイポによるエラーが検出されるので修正がしやするなる。typeは一箇所にまとめて記述するのが通例
+
 export const DELETE_EVENT = "DELETE_EVENT"
 export const CREATE_EVENT = "CREATE_EVENT"
 export const DELETE_ALL_EVENT = "DELETE_ALL_EVENT"
